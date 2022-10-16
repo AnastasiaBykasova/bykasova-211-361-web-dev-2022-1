830df20f849efc2286194ef40dbc49aa035db1a6
Anastasia Bykasova <nastybykasova@gmail.com>
2022-10-16 16:41:26 +0300

    adding feedback saving with PHP

--- home.php
+++ home.php
@@ -2,7 +2,7 @@
 <html lang="ru">
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="feedback_style.css">
+    <link rel="stylesheet" href="home_style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Nabla&family=Open+Sans:ital,wght@0,400;0,500;1,400;1,500&family=Poppins&display=swap" rel="stylesheet">
@@ -14,7 +14,6 @@
             <div class="container">
                 <div class="header-line">
                     <div class="nav">
-                        <!--<a data-content="Homepage" class="my-page" href="index.php">Homepage &#9749;</a>-->
                         <a data-content="Homepage" href="<?php 
                             $name='Homepage &#9749;'; 
                             $link='index.php'; 
@@ -61,39 +60,23 @@
     <main>
         <div class="background-fb">
             <div class="container-fb-form">
-            <form class="feedback_form" action="home.php" method="post" name="feedback_form">
                 <h1 class="name-fb">Feedback</h1>
-                <!--<p><a class="hp-back" href="index.html"><i>На главную</i> &#8617;</a></p>-->
                 <div class="elements-fb">
-                    <p>ФИО: <input type="text" name="name-input"placeholder="Введите фамилию, имя и отчество" size="40" value="<?php if (isset($_GET['surname-input'])){
-                        echo $_GET['surname-input']; }?>"/></p>
-                    <p>Email: <input type="email" name="email-input"placeholder="Введите email" size="30" value="<?php if (isset($_GET['email-input'])){
-                        echo $_GET['email-input']; }?>"/></p>
-                    <div class="from-where">
-                        <p>Откуда узнали о нас? </p>
-                        <p><input type="radio" name="type-input" value="1" <?php if (isset($_GET['source']) & $_GET['source']='1'){
-                        echo 'checked';}?>/>В школе/в университете/на работе</p>
-                        <p><input type="radio" name="type-input" value="2" <?php if (isset($_GET['source']) & $_GET['source']='2'){
-                        echo 'checked';}?>/>Из социальных сетей</p>
-                        <p><input type="radio" name="type-input" value="3" <?php if (isset($_GET['source']) & $_GET['source']='3'){
-                        echo 'checked';}?>/>От семьи/друзей/знакомых</p>
-                        <p><input type="radio" name="type-input" value="4" <?php if (isset($_GET['source']) & $_GET['source']='4'){
-                        echo 'checked';}?>/>Из рекламы</p>
-                    </div>
-                    <p>Тип обращения: 
-                    <select sixe="1" name="letter-type" id="letter-type">
-                        <option value=”1”>Предложение</option>
-                        <option value=”2”>Жалоба</option>                        
-                    </select></p>
-                    <div class="appeal-text">
-                        <p>Текст сообщения: </p>
-                        <textarea name="tell-us-message" placeholder="Введите текст" rows="5" wrap="soft"></textarea>
-                    </div>
-                    <p>Вложения: <input type="file" name="file-input" multiple></p>
-                    <p>Даю согласие на обработку персональных данных <input type="checkbox"></p>
-                    <p><div class="send-button"><input type="submit"></div></p>
+                    <?php
+                    echo '<p class="hello-fb"> Здравствуйте, '.$_POST['name-input'].'!</p>';
+                    if ($_POST['letter-type'] == '1') {
+                        echo '<p>Спасибо за ваше предложение!</p>';
+                        echo '<textarea>'.$_POST['tell-us-message'].'</textarea>';
+                    }else {
+                        echo '<p>Мы рассмотрим Вашу жалобу:</p>';
+                        echo '<textarea>'.$_POST['tell-us-message'].'</textarea>';
+                    }
+                    if (isset($_POST['file-input']) & $_POST['file-input'] != '') {
+                        echo '<p>Вы приложили следующий файл: '.$_POST['file-input'].'</p>';
+                    }
+                    echo '<p><a class="rewrite-fb" href="feedback.php?name-input='.$_POST['name-input'].'&email-input='.$_POST['email-input'].'&type-input='.$_POST['type-input'].'"><i>Заполнить снова</i></a></p>';
+                    ?>
                 </div>
-            </form>
             </div>
         </div>
     </main>
